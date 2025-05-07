87b9d1724fa3265897b4c0d249c81bd0042c4a4f
Devarsh07 <debarshupadhyay7@gmail.com>
2025-05-07 21:16:40 +0530

    build

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
